c0e7122851aa19b791b2f57ace5cc47eb88f2b0f
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-13 00:04:37 +0530

    implemened stack ds

---
+++ Stackpkg/Main.java
@@ -0,0 +1,17 @@
+package Stackpkg;
+
+public class Main {
+public static void main(String[] args) {
+		
+		Stack stack = new Stack();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		stack.display();
+		stack.pop();
+		stack.display();
+		System.out.println("Stack top is "+stack.peek());
+
+	}
+
+}
